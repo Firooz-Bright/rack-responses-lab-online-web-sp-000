6aae80ad9de8c5fa225d44b43dfdaae206abe6cc
Firooz Mafakheri <firooz.mafakheri@gmail.com>
2020-06-12 23:10:50 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     time= Time.now.to_i
-    if time == 12 puts 
+    if time == 12 puts "Good Morning!"
     resp.write 
     resp.finish
   end
