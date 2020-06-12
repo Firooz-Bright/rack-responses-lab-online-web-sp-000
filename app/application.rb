9ad839a06853b97b7df270839b1c720179c137e1
Firooz Mafakheri <firooz.mafakheri@gmail.com>
2020-06-12 23:30:06 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,17 +1,18 @@
 class Application
-
+ 
   def call(env)
     resp = Rack::Response.new
-
     time = Time.now
-
-    if time.hour <= 12
-      resp.write "Good Morning!"
-    else
-      resp.write "Good Afternoon!"
-    end
-
+    
+    if Time.new <= time
+     resp.write "Good Morning!"
+     
+   else
+      
+     
+    resp.write "Good Afternoon!"
+   
     resp.finish
   end
-
+ end 
 end
