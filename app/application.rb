dd921261eab93c47120a290eb2aace18dc69f3d1
Firooz Mafakheri <firooz.mafakheri@gmail.com>
2020-06-12 23:13:47 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
     if time.hour < 12 
       puts "Good Morning!"
   else
-    if time.hour >12 
+     time.hour >12 
       puts "Good Afternoon!"
     resp.write "#{time.hour}\n"
     resp.finish
